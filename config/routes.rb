bcd79c0d42c40c7fb04592baf21f160a2ed238be
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-04-26 11:19:23 +1200

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
+  get 'homepage/index'
+
   resources :users
   resources :players
   resources :pieces
-  root 'games#index'
   resources :games
 end
