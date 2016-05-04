59f27d234a1116866a3cccb6f4abc24a4fce4863
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-04 12:11:58 +1200

    Fix features on max users

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   has_many :players, dependent: :destroy
   validates_each :players do |game, attr, value|
-    game.errors.add :base, :game_is_full if game.players.size >= game.number_of_players
+    game.errors.add :base, :game_is_full if game.players.size > game.number_of_players
   end
   validates :number_of_players, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: Player.pieces.count }
 end
