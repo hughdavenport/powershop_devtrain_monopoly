ed604da972724167a74a001817d115fdce837fa5
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-09 09:51:13 +1200

    Add specs to pass game view

--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "games/show", type: :view do
   before(:each) do
     @game = assign(:game, Game.create!(number_of_players: 2))
+    user = assign(:current_user, User.create!(username: "testing"))
+    @game.players.create!(user: user, piece: :wheelbarrow)
   end
 
   it "renders attributes in <p>" do
