b12e0933168c4236a29170d4706bb71a30dba03f
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-12 15:02:22 +1200

    Add steps for new game features

--- features/step_definitions/playing_a_game_steps.rb
+++ features/step_definitions/playing_a_game_steps.rb
@@ -1,8 +1,46 @@
 DICE_ROLL_SELECTOR = '#diceroll'
+DICE_ROLL_REGEX    = /^You rolled a (?<dice_roll>\d+)$/i
+
+def dice_roll
+  find(DICE_ROLL_SELECTOR).text.gsub(DICE_ROLL_REGEX, '\\k<dice_roll>')
+end
+
+Given(/^It is my turn$/) do
+  # The background should set this up
+  step "I go to the game"
+end
+
 
 When(/^I roll the dice$/) do
   step 'I click on "Roll Dice"'
 end
+
+When(/^I roll two dice$/) do
+  2.times { step 'I roll the dice' }
+end
+
+When(/^I roll two dice \(not doubles\)$/) do
+  step 'I roll the dice'
+            # make 0-based for % operation, then 1-based for step
+  step "I roll a #{(((dice_roll.to_i - 1) + 1) % 6) + 1}"
+end
+
+When(/^I roll (a|\d+) (?:double|doubles)$/) do |number|
+  number = 1 if number == "a"
+  number.to_i.times do
+    step 'I roll the dice'
+    step "I roll a #{dice_roll}"
+  end
+end
+
+When(/^I roll a (\d+)$/) do |number|
+  # Need to create the dice manually here...
+  # Add to the game events model?
+  # or change controller to accept number when in testing mode...
+  step "I enter in #{number} as dice roll"
+  step 'I roll the dice'
+end
+
 
 Then(/^I should see a dice roll$/) do
   expect(page).to have_selector(DICE_ROLL_SELECTOR)
