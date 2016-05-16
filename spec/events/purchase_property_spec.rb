dd557861d19579a88fbff68fed018f6e15a77af4
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-17 11:02:08 +1200

    Mock out purchase property service and stub the event spec

---
+++ spec/events/purchase_property_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseProperty, type: :event do
+  pending "Test avail money"
+  pending "Test owned and not"
+  pending "Test property and not"
+  pending "Test diff types of prop?"
+  pending "Test my turn or not"
+end
