84a05e1c1661ac6397a092a2f94ba756adf4182b
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-17 10:31:57 +1200

    Move events to a root folder

--- app/events/dice_roll.rb
+++ app/events/dice_roll.rb
@@ -3,6 +3,7 @@ class DiceRoll < Event
   store_accessor :data, :amount
 
   def apply(game_state)
+    # get rid of taps
     game_state.tap do |game_state|
       game_state.players[game_state.current_player].tap do |player|
         player[:dice_rolls] << amount.to_i
--- app/events/pay_rent.rb
+++ app/events/pay_rent.rb
@@ -2,6 +2,7 @@ class PayRent < Event
   def apply(game_state)
     game_state.tap do |game_state|
       game_state.players[game_state.current_player].tap do |player|
+        # make a query object to just get what I need, rather than multiple calls like this
         property = game_state.board[player[:location]]
         details = game_state.details(property)
         owner = game_state.property_owner(property)
