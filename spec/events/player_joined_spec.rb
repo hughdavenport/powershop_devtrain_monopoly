c9c09cd8895d4740dcef8afcbb4dede818f31f61
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-17 10:32:47 +1200

    Start adding basics of event specs

---
+++ spec/events/player_joined_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe PlayerJoined, type: :event do
+  pending "Test no players in game passes"
+  pending "Test same piece fails"
+  pending "Test last player passes"
+  pending "Testing full game fails"
+  pending "Test invalid piece fails?"
+end
