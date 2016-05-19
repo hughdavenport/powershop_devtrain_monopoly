6fa18de5eca02c843d99a0b83b1696a9b76339f3
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-20 10:20:24 +1200

    Add house prices to seed data

--- app/squares/coloured_property.rb
+++ app/squares/coloured_property.rb
@@ -7,6 +7,11 @@ class ColouredProperty < Property
   validates_presence_of :rent
   validates :rent, numericality: { only_integer: true, greater_than: 0 }
 
+  store_accessor :data, :house_rent
+  validates_presence_of :house_rent
+  validates :house_rent, length: { is: 4 }
+  validate :validate_house_rent
+
   def house_price
     {
       brown: 50,
@@ -18,5 +23,13 @@ class ColouredProperty < Property
       green: 200,
       purple: 200,
     }.fetch(colour)
+  end
+
+  private
+
+  def validate_house_rent
+    if !house_rent.is_a?(Array) || house_rent.detect { |rent| !rent.is_a?(Integer) || rent <= 0 }
+      errors.add(:house_rent, :invalid)
+    end
   end
 end
