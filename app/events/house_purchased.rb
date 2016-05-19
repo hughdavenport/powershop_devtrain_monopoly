77c5b0ddac12434f440d9c51e5750671914f7091
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-19 18:18:18 +1200

    Whoops, broke rpsec, fixed now

--- app/events/house_purchased.rb
+++ app/events/house_purchased.rb
@@ -14,6 +14,7 @@ class HousePurchased < Event
   def errors(game_state = game.state)
     player = game_state.players[game_state.current_player]
     ActiveModel::Errors.new(self).tap do |errors|
+      errors.add(:base, :already_rolled) unless game_state.expecting_rolls == 2
       errors.add(:base, :cannot_buy_house_on_property) unless player.can_buy_house?(property)
       errors.add(:base, :not_enough_money) unless player.can_afford?(ColouredProperty.find_by_name(property).house_price)
     end
--- spec/events/house_purchased_spec.rb
+++ spec/events/house_purchased_spec.rb
@@ -15,24 +15,14 @@ RSpec.describe HousePurchased, type: :event do
     before do
       expect(game_state).to receive(:current_player).and_return(0)
       expect(game_state).to receive(:players).and_return([player])
+      expect(game_state).to receive(:expecting_rolls).and_return(expecting_rolls)
     end
 
-    let(:house) do
-      instance_double("House").tap do |house|
-        expect(house).to receive(:house_price).and_return(price)
-      end
-    end
-
-    let(:player) do
-      instance_double("Player").tap do |player|
-        expect(player).to receive(:can_buy_house?).and_return(can_buy_house)
-      end
-    end
-
+    let(:player) { instance_double("Player") }
     let(:price) { 50 }
 
-    context "we are not allowed to buy a house" do
-      let(:can_buy_house) { false }
+    context "we have already rolled" do
+      let(:expecting_rolls) { 0 }
 
       describe "#can_apply?" do
         subject { event.can_apply?(game_state) }
@@ -43,10 +33,14 @@ RSpec.describe HousePurchased, type: :event do
       describe "#errors" do
         before do
           expect(player).to receive(:can_afford?).with(price).and_return(true)
+          expect(player).to receive(:can_buy_house?).and_return(true)
+          expect(house).to receive(:house_price).and_return(price)
           class_double("ColouredProperty").as_stubbed_const.tap do |coloured_property|
             expect(coloured_property).to receive(:find_by_name).with(property).and_return(house)
           end
         end
+
+        let(:house) { instance_double("House") }
 
         subject { event.errors(game_state) }
 
@@ -54,18 +48,21 @@ RSpec.describe HousePurchased, type: :event do
       end
     end
 
-    context "we are allowed to buy a house" do
-      let(:can_buy_house) { true }
+    context "we have not rolled this turn" do
+      let(:expecting_rolls) { 2 }
 
       before do
-        expect(player).to receive(:can_afford?).with(price).and_return(can_afford)
-        class_double("ColouredProperty").as_stubbed_const.tap do |coloured_property|
-          expect(coloured_property).to receive(:find_by_name).with(property).and_return(house)
+        expect(player).to receive(:can_buy_house?).and_return(can_buy_house)
+      end
+
+      let(:house) do
+        instance_double("House").tap do |house|
+          expect(house).to receive(:house_price).and_return(price)
         end
       end
 
-      context "but we can't afford to buy the house" do
-        let(:can_afford) { false }
+      context "we are not allowed to buy a house" do
+        let(:can_buy_house) { false }
 
         describe "#can_apply?" do
           subject { event.can_apply?(game_state) }
@@ -74,25 +71,59 @@ RSpec.describe HousePurchased, type: :event do
         end
 
         describe "#errors" do
+          before do
+            expect(player).to receive(:can_afford?).with(price).and_return(true)
+            class_double("ColouredProperty").as_stubbed_const.tap do |coloured_property|
+              expect(coloured_property).to receive(:find_by_name).with(property).and_return(house)
+            end
+          end
+
           subject { event.errors(game_state) }
 
           it { is_expected.to be_present }
         end
       end
 
-      context "and we can afford to buy the house" do
-        let(:can_afford) { true }
+      context "we are allowed to buy a house" do
+        let(:can_buy_house) { true }
 
-        describe "#can_apply?" do
-          subject { event.can_apply?(game_state) }
-
-          it { is_expected.to be_truthy }
+        before do
+          expect(player).to receive(:can_afford?).with(price).and_return(can_afford)
+          class_double("ColouredProperty").as_stubbed_const.tap do |coloured_property|
+            expect(coloured_property).to receive(:find_by_name).with(property).and_return(house)
+          end
         end
 
-        describe "#errors" do
-          subject { event.errors(game_state) }
+        context "but we can't afford to buy the house" do
+          let(:can_afford) { false }
 
-          it { is_expected.not_to be_present }
+          describe "#can_apply?" do
+            subject { event.can_apply?(game_state) }
+
+            it { is_expected.to be_falsey }
+          end
+
+          describe "#errors" do
+            subject { event.errors(game_state) }
+
+            it { is_expected.to be_present }
+          end
+        end
+
+        context "and we can afford to buy the house" do
+          let(:can_afford) { true }
+
+          describe "#can_apply?" do
+            subject { event.can_apply?(game_state) }
+
+            it { is_expected.to be_truthy }
+          end
+
+          describe "#errors" do
+            subject { event.errors(game_state) }
+
+            it { is_expected.not_to be_present }
+          end
         end
       end
     end
