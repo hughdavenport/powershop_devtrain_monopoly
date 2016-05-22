f22dc453230c6c1e65248c84a2bc6d9a37b23d34
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-23 10:06:19 +1200

    Test affordability of paying bond

--- app/events/bond_paid.rb
+++ app/events/bond_paid.rb
@@ -4,12 +4,15 @@ class BondPaid < Event
   end
 
   def can_apply?(game_state = game.state)
-    game_state.expecting_rolls == 2
+    player = game_state.players[game_state.current_player]
+    game_state.expecting_rolls == 2 && player.can_afford?(50)
   end
 
   def errors(game_state = game.state)
+    player = game_state.players[game_state.current_player]
     ActiveModel::Errors.new(self).tap do |errors|
       errors.add(:base, :already_rolled) unless game_state.expecting_rolls == 2
+      errors.add(:base, :cannot_afford) unless player.can_afford?(50)
     end
   end
 end
