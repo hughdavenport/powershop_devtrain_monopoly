5d8e48a36025b77e9850489b8afe01f077044c81
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-05-24 10:39:21 +1200

    Let you pick a random chance card
    Note, resets each time, need to save the state

--- app/events/card_drawn.rb
+++ app/events/card_drawn.rb
@@ -1,8 +1,9 @@
 class CardDrawn < Event
-  after_initialize :default_values
   store_accessor :data, :card
 
   def apply(game_state)
+    # TODO make this more reliable
+    self.card = game_state.cards.sample.name unless card.present?
     unless card == "No card"
       # TODO get card, apply it
       card_obj = game_state.cards.find_by_name(card)
@@ -19,11 +20,5 @@ class CardDrawn < Event
     ActiveModel::Errors.new(self).tap do |errors|
       errors.add(:base, :not_expecting_card_draw) unless game_state.expecting_card_draw
     end
-  end
-
-  private
-
-  def default_values
-    self.card = "TODO, DRAW A CARD" unless card.present?
   end
 end
